9830131a53d84b387d391d6773d44135adb896db
leokim <kunga077@gmail.com>
2023-06-15 23:51:45 +0900

    sleep 시간 변경

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,10 +1,11 @@
 echo "start brokers"
 docker-compose -f ./docker-compose-three-brokers.yml up -d
 
-echo "this takes time about 15 seconds to execute servers"
+echo "this takes time about 30 seconds to execute servers"
 
-sleep 40
+sleep 30
 
+echo "create topic 'test'"
 docker exec broker-1 kafka-topics --create --topic test --bootstrap-server broker-1:9092 --replication-factor 3 --partitions 3
 
 echo "completed"
