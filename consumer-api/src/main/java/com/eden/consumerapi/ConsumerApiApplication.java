93b1c325437a10266ee80f1fd122afb91a95f04d
leokim <kunga077@gmail.com>
2023-06-15 22:59:29 +0900

    first commit

---
+++ consumer-api/src/main/java/com/eden/consumerapi/ConsumerApiApplication.java
@@ -0,0 +1,12 @@
+package com.eden.consumerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApiApplication.class, args);
+    }
+}
